5f296709f2855d43a2dd95aefab4465b831a6605
DeadKesar <50805926+DeadKesar@users.noreply.github.com>
2021-10-28 09:31:29 +0300

    отлаженно

--- лаб 3/лаб 3.cpp
+++ лаб 3/лаб 3.cpp
@@ -1,142 +1,161 @@
 ﻿//TODO: написать функцию сортировки массива.
-
 #include <iostream>
+#include <iomanip>
 
+//заполняем мвссив случайными числами
 int* CreateArray(int size);
-void SortArray(int* arr, int lenght,int start);
+//рекурсивный вызов сортировки
+void SortArray(int* arr, int lenght,int start = 0);
+//сортирует нечётные длины
+void SortOddLength(int lenght, int start, int* arr);
+//сортирует чётные длины
+void SortEvenLength(int lenght, int start, int* arr);
+// печатаем наш массив
+void PrintArr(const int arr[], int size);
 
 using namespace std;
 
 int main()
 {
-	int const SIZE = 10;
-	//int* arr = CreateArray(SIZE);
-	int arr[]{ 1,5,2,3,4,7,9,8,2,1 };
-	for (int i = 0; i < SIZE; i++)
-	{
-		cout << arr[i] << ", ";
-	}
-	cout << endl<< "_______________"<< endl;
-
-	SortArray(arr, SIZE,0);
-   	for (int i = 0; i < SIZE; i++)
-	{
-		cout << arr[i] << ", ";
-	}
-	cout << endl;
+	int const SIZE = 100;//количество сортируемых чисел
+	int* arr = CreateArray(SIZE);
+	//выводим числа до сортировки
+	PrintArr(arr, SIZE);
+	cout << endl << "_______________" << endl;
+	SortArray(arr, SIZE);
+	//выводим числа после сортировки
+	PrintArr(arr, SIZE);	delete[] arr;
+	return 0;
 }
-
 //заполняет масив случайными числами.
 int* CreateArray(int size)
 {
 	srand(static_cast<int>(time(0)));
-	int* arr = new int[size];
+	int *arr = new int[size]();
 	for (size_t i = 0; i < size; i++)
 	{
 		arr[i] = rand();
 	}
 	return arr;
 }
-
-void SortArray(int* arr, int lenght, int start = 0)
+//рекурсивный вызов сортировки
+void SortArray(int* arr, int length, int start)
 {
-	if (lenght > 1)
+	if (length > 1)
 	{
-		if (lenght % 2 == 0)
+		if (length % 2 == 0)
 		{
-			SortArray(arr, lenght / 2, start);
-			SortArray(arr, lenght / 2, lenght / 2);
+			SortArray(arr, length / 2, start);	//идём влево
+			SortArray(arr, length / 2, start + length / 2);	 //идём вправо
+			return SortEvenLength(length, start, arr);
 		}
 		else
 		{
-			SortArray(arr, lenght / 2, start);
-			SortArray(arr, lenght / 2 + 1, lenght / 2+1);
+			SortArray(arr, length / 2, start); //идём влево
+			SortArray(arr, length / 2 + 1, start + length / 2); //идём вправо
+			return SortOddLength(length, start, arr);
 		}
 	}
-	else
+}
+//сортирует нечётные длины
+void SortOddLength(int length, int start, int* arr)
+{
+	int* tempArr = new int[length];
+	for (int tempIndex = 0, leftIndex = start, rightIndex = start + length / 2;
+		tempIndex < length; tempIndex++)
 	{
-		return;
-	}
-	if (lenght % 2 == 0)
-	{
-		int* tempArr = new int[lenght];
-		for (int tempIndex = 0, leftIndex = start, rightIndex = start+lenght / 2; tempIndex < lenght; tempIndex++)
+		if (arr[leftIndex] <= arr[rightIndex])
 		{
-			if (arr[leftIndex] <= arr[rightIndex])
-			{
-				tempArr[tempIndex] = arr[leftIndex];
-				leftIndex++;
-			}
-			else
+			tempArr[tempIndex] = arr[leftIndex];
+			leftIndex++;
+		}
+		else
+		{
+			tempArr[tempIndex] = arr[rightIndex];
+			rightIndex++;
+		}
+		if (leftIndex >= start + length / 2)
+		{
+			tempIndex++;
+			for (tempIndex; tempIndex < length; tempIndex++)
 			{
 				tempArr[tempIndex] = arr[rightIndex];
 				rightIndex++;
 			}
-			if (leftIndex >= start + lenght / 2)
-			{
-				tempIndex++;
-				for (tempIndex; tempIndex < lenght; tempIndex++)
-				{
-					tempArr[tempIndex] = arr[rightIndex];
-					rightIndex++;
-				}
-			}
-			if (rightIndex >= start + lenght / 2 && tempIndex < lenght)
-			{
-				tempIndex++;
-				for (tempIndex; tempIndex < lenght; tempIndex++)
-				{
-					tempArr[tempIndex] = arr[leftIndex];
-					leftIndex++;
-				}
-			}
 		}
-		for (int baseIndex = start, tempIndex = 0; baseIndex < lenght; baseIndex++, tempIndex++)
+		if (rightIndex >= start + length && tempIndex < length)
 		{
-			arr[baseIndex] = tempArr[tempIndex];
-		}
-		delete[] tempArr;
-		return;
-	}
-	else
-	{
-		int* tempArr = new int[lenght];
-		for (int tempIndex = 0, leftIndex = start, rightIndex = start + lenght / 2 + 1; tempIndex < lenght; tempIndex++)
-		{
-			if (arr[leftIndex] >= arr[rightIndex])
+			tempIndex++;
+			for (tempIndex; tempIndex < length; tempIndex++)
 			{
 				tempArr[tempIndex] = arr[leftIndex];
 				leftIndex++;
 			}
-			else
+		}
+	}
+	for (int baseIndex = start, tempIndex = 0; tempIndex < length;
+		baseIndex++, tempIndex++)
+	{
+		arr[baseIndex] = tempArr[tempIndex];
+	}
+	delete[] tempArr;
+	return;
+}
+//сортирует чётные длины
+void SortEvenLength(int length, int start, int* arr)
+{
+	int *tempArr = new int[length];
+	for (int tempIndex = 0, leftIndex = start, rightIndex = start + length / 2;
+		tempIndex < length; tempIndex++)
+	{
+		if (arr[leftIndex] <= arr[rightIndex])
+		{
+			tempArr[tempIndex] = arr[leftIndex];
+			leftIndex++;
+		}
+		else
+		{
+			tempArr[tempIndex] = arr[rightIndex];
+			rightIndex++;
+		}
+		if (leftIndex >= start + length / 2)
+		{
+			tempIndex++;
+			for (tempIndex; tempIndex < length; tempIndex++)
 			{
 				tempArr[tempIndex] = arr[rightIndex];
 				rightIndex++;
 			}
-			if (leftIndex >= start + lenght / 2)
-			{
-				tempIndex++;
-				for (tempIndex; tempIndex < lenght; tempIndex++)
-				{
-					tempArr[tempIndex] = arr[rightIndex];
-					rightIndex++;
-				}
-			}
-			if (rightIndex >= start + lenght / 2+1 && tempIndex < lenght)
-			{
-				tempIndex++;
-				for (tempIndex; tempIndex < lenght; tempIndex++)
-				{
-					tempArr[tempIndex] = arr[leftIndex];
-					leftIndex++;
-				}
-			}
 		}
-		for (int baseIndex = start, tempIndex = 0; baseIndex < lenght; baseIndex++, tempIndex++)
+		if (rightIndex >= start + length && tempIndex < length)
 		{
-			arr[baseIndex] = tempArr[tempIndex];
+			tempIndex++;
+			for (tempIndex; tempIndex < length; tempIndex++)
+			{
+				tempArr[tempIndex] = arr[leftIndex];
+				leftIndex++;
+			}
 		}
-		delete[] tempArr;
-		return;
+
 	}
+	for (int baseIndex = start, tempIndex = 0; tempIndex < length;
+		baseIndex++, tempIndex++)
+	{
+		arr[baseIndex] = tempArr[tempIndex];
+	}
+	delete[] tempArr;
+	return;
+}
+// выводим массив на экран
+void PrintArr(const int arr[], int size)
+{
+	int count = 1;
+	for (int i = 0; i < size; i++)
+	{
+		cout << setw(5) << arr[i] << "| ";
+		if (count % 20 == 0)
+			cout << endl;
+		count++;
+	}
+	cout << endl;
 }
